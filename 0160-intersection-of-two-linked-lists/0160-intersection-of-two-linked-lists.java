e9e536e2353f5e3c058a564054890219cb908e2c
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-07-28 17:31:25 +0530

    Time: 1 ms (99.93%), Space: 53.1 MB (12.53%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,36 +10,35 @@
  * }
  */
 public class Solution {
-    public int getLength(ListNode head) {
-        int count = 0;
-        while (head != null) {
-            count++;
-            head = head.next;
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        int len1 = 0;
+        ListNode temp1 = head1;
+        while(temp1 != null){
+            temp1 = temp1.next;
+            len1++;
         }
-        return count;
-    }
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int lenA = getLength(headA);
-        int lenB = getLength(headB);
-        int diff = Math.abs(lenA - lenB);
-        if (lenA > lenB) {
-            while (diff > 0) {
-                headA = headA.next;
-                diff--;
+
+        int len2 = 0;
+        ListNode temp2 = head2;
+        while(temp2 != null){
+            temp2 = temp2.next;
+            len2++;
+        }
+        temp1 = head1;
+        temp2 = head2;
+        if(len1>len2){
+            for(int i=0;i<len1-len2;i++){
+                temp1 = temp1.next;
             }
-        } else {
-            while (diff > 0) {
-                headB = headB.next;
-                diff--;
+        }else{
+            for(int i=0;i<len2-len1;i++){
+                temp2 = temp2.next;
             }
         }
-        while (headA != null && headB != null) {
-            if (headA == headB) {
-                return headA;
-            }
-            headA = headA.next;
-            headB = headB.next;
+        while(temp1 != temp2){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
         }
-        return null;
+        return temp1;
     }
 }
