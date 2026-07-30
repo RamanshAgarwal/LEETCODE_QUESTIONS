d40025c4094e859de65428ccd88e84d9648c4463
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-07-30 12:26:04 +0530

    Time: 3 ms (99.82%), Space: 93.9 MB (97.23%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,48 +1,48 @@
-// class Solution {
-//     public boolean isPalindrome(ListNode head) {
-//         if (head == null || head.next == null)
-//          return true;
-//         ListNode slow = head;
-//         ListNode fast = head;
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//         }
-//         ListNode prev = null;
-//         ListNode curr = slow;
-//         while (curr != null) {
-//             ListNode next = curr.next;
-//             curr.next = prev;
-//             prev = curr;
-//             curr = next;
-//         }
-//         ListNode left = head;
-//         ListNode right = prev;
-//         while (right != null) {
-//             if (left.val != right.val)
-//                 return false;
-//             left = left.next;
-//             right = right.next;
-//         }
-//         return true;
-//     }
-// }
 class Solution {
-    public boolean isPalindrome(ListNode head) { 
-        ArrayList<Integer> list = new ArrayList<>();
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+         return true;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int i = 0;
-        int j = list.size() - 1;
-        while (i < j) {
-            if (!list.get(i).equals(list.get(j))) {
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode left = head;
+        ListNode right = prev;
+        while (right != null) {
+            if (left.val != right.val)
                 return false;
-            }
-            i++;
-            j--;
+            left = left.next;
+            right = right.next;
         }
         return true;
     }
 }
+// class Solution {
+//     public boolean isPalindrome(ListNode head) { 
+//         ArrayList<Integer> list = new ArrayList<>();
+//         while (head != null) {
+//             list.add(head.val);
+//             head = head.next;
+//         }
+//         int i = 0;
+//         int j = list.size() - 1;
+//         while (i < j) {
+//             if (!list.get(i).equals(list.get(j))) {
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
