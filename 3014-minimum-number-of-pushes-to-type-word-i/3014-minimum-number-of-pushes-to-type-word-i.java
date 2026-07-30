26787ff345d1c238816228bade399b07827f30ea
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-07-30 10:40:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minimumPushes(String word) {
-          int n = word.length();
+        int n = word.length();
         int ans = 0;
 
         for (int i = 0; i < n; i++) {
