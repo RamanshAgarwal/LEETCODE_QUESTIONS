847427e10ba487e5636eb464fd1e020cdee5c482
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-08-03 23:43:15 +0530

    Time: 46 ms (43.47%), Space: 52.9 MB (85.87%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+
+    return {
+        toBe: function(other) {
+            if(val !== other) {
+                throw new Error("Not Equal");
+            }
+            return true;
+        },
+
+        notToBe: function(other) {
+            if(val === other) {
+                throw new Error("Equal");
+            }
+            return true;
+        }
+    };
+};
