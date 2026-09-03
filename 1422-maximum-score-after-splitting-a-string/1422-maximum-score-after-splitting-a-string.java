df9a7c4b473934ef350c6dc05ebe7ad28ef4e812
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-09-03 19:33:58 +0530

    Time: 1 ms (99.27%), Space: 42.7 MB (65.73%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxScore(String s) {
         int n = s.length();
-        int maxScore = 0;
-        for(int i=0;i<n-1;i++){
-            int leftZeroes = 0;
-            int rightOnes = 0;
-            for(int j=0;j<=i;j++){
-                if(s.charAt(j) == '0'){
-                    leftZeroes++;
-                }
+        int rightOnes = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                rightOnes++;
             }
-            for(int j=i+1;j<n;j++){
-                if(s.charAt(j) == '1'){
-                    rightOnes++;
-                }
+        }
+        int leftZeroes = 0;
+        int maxScore = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeroes++;
+            } else {
+                rightOnes--;
             }
             int score = leftZeroes + rightOnes;
-            maxScore = Math.max(maxScore,score);
+            maxScore = Math.max(maxScore, score);
         }
         return maxScore;
     }
