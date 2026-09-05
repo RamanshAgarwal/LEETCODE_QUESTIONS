2eb8bc2d867b5ba94daeec5151d3421fdb9ef65a
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-09-05 10:41:34 +0530

    Time: 15 ms (60.07%), Space: 51.3 MB (87.37%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -4,16 +4,12 @@ class Solution {
         Arrays.sort(s);
         int i=0;
         int j=0;
-        int count=0;
         while(i<g.length && j<s.length){
             if(g[i]<=s[j]){
                 i++;
-                j++;
-                count++;
-            }else{
-            j++;
             }
+            j++;
         }
-        return count;
+        return i;
     }
 }
