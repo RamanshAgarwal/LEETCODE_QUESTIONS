d22d94d2c8f52af485cc526346a6ecaccebdf3c0
Ramansh Agarwal <ramanshagarwal3@gmail.com>
2026-09-14 11:36:04 +0530

    Time: 36 ms (28.22%), Space: 58.6 MB (94.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    void TwoSum(int[] nums,int target,int i,int j){
-        while(i<j){
-            if(nums[i]+nums[j] == target){
-                ans.add(Arrays.asList(nums[i],nums[j],-target));
-                while(i<j && nums[i] == nums[i+1]) i++;
-                while(i<j && nums[j] == nums[j-1]) j--;
-                i++;
-                j--;
-            }
-            else if(nums[i] + nums[j] < target){
-                i++;
-            }
-            else{
-                j--;
-            }
-        }
-    }
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
-        if(n<3) return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n-2;i++){
-           if(i>0 &&nums[i] == nums[i-1]) continue;
-           int n1 = nums[i];
-           int target = -n1;
-           TwoSum(nums,target,i+1,n-1);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == -nums[i]) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < -(nums[i])) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
         }
         return ans;
     }
